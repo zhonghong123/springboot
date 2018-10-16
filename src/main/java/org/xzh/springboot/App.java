f014eddeae8207b5fa787101c748d69b088078b1
xzh <624170443@qq.com>
2018-10-16 14:31:08 +0800

    init springboot project

---
+++ src/main/java/org/xzh/springboot/App.java
@@ -0,0 +1,13 @@
+package org.xzh.springboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
