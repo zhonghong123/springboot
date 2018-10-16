4eb78ff441414cb413d3793a1dd41097be033a66
xzh <624170443@qq.com>
2018-10-16 14:59:19 +0800

    init project

---
+++ src/main/java/org/xzh/springboot/MainApplication.java
@@ -0,0 +1,17 @@
+package org.xzh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class MainApplication 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
