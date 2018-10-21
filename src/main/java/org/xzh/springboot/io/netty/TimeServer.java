1f7b03c0049f9ec3f7f01f8234c0c77b4634c64c
misuosi <misuosi@126.com>
2018-10-21 22:33:49 +0800

    FramedDecoder

--- src/main/java/org/xzh/springboot/io/netty/TimeServer.java
+++ src/main/java/org/xzh/springboot/io/netty/TimeServer.java
@@ -7,6 +7,8 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 public class TimeServer {
 
@@ -41,6 +43,10 @@ public class TimeServer {
 
 		@Override
 		protected void initChannel(SocketChannel arg0) throws Exception {
+			//解决粘包问题
+			//arg0.pipeline().addLast(new LineBasedFrameDecoder(1024));
+			//字符串解码器，在handler收到就是String形式
+			//arg0.pipeline().addLast(new StringDecoder());
 			//设置handler
 			arg0.pipeline().addLast(new TimeServerHandler());
 		}
