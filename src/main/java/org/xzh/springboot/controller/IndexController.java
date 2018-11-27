3c54683600c8d19703aeea5ced8b52ac6bdb85a8
xzh <624170443@qq.com>
2018-11-27 19:22:47 +0800

    bootstrap

--- src/main/java/org/xzh/springboot/controller/IndexController.java
+++ src/main/java/org/xzh/springboot/controller/IndexController.java
@@ -3,6 +3,7 @@ package org.xzh.springboot.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,8 +13,9 @@ public class IndexController {
 	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
 	@RequestMapping("/toIndex")
-	public String index(){
+	public String index(Model model){
 		logger.info("index...");
+		model.addAttribute("text", "hello world");
 		return "index";
 	}
 }
