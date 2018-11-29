694481831b20eeaa2877715e35c18ff4d4975401
xzh <624170443@qq.com>
2018-11-29 17:46:28 +0800

    ex

--- src/main/java/org/xzh/springboot/controller/IndexController.java
+++ src/main/java/org/xzh/springboot/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +18,17 @@ public class IndexController {
 		logger.info("index...");
 		model.addAttribute("text", "hello world");
 		return "index";
+	}
+	
+	@RequestMapping("/admin/{path}")
+	public String admin(@PathVariable String path){
+		logger.info("path --> {}", path);
+		return "admin/"+path;
+	}
+	
+	@RequestMapping("/front/{path}")
+	public String front(@PathVariable String path){
+		logger.info("front path --> {}", path);
+		return "front/"+path;
 	}
 }
